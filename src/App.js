b85c1a10ab6b051eb7cd6a5c45a020c484bfaad7
Eduado Varella <eduardo.varella@bizcap.com.br>
2018-07-12 09:19:51 -0300

    Get books on shelfs from backend and show as Book component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 import HomeScreen from './components/screens/HomeScreen';
 import SearchScreen from './components/screens/SearchScreen';
 import { Route } from 'react-router-dom';
 
+
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
+    books: []
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+        this.setState({ books });
+    });
+  }
+
+  updateBook = (book) => {
+    console.log(book);
   }
 
   render() {
     return (
       <div className="app">
         <Route exact path="/" render={() => (
-          <HomeScreen />
+          <HomeScreen books={ this.state.books } onBookUpdate={this.updateBook} />
         )}/>
         <Route path="/search" render={() => (
           <SearchScreen />
